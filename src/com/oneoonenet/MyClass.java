0d09176b4653eccb416ab2341c9da68afe4f5ce2
meeuwsstefaan <stefaanmeeuws@gmail.com>
2021-11-06 09:29:23 +0100

    Initial commit.

---
+++ src/com/oneoonenet/MyClass.java
@@ -0,0 +1,29 @@
+package com.oneoonenet;
+
+import java.util.Scanner;
+
+public class MyClass {
+    public boolean isPrime(int n) {
+        for (int i = 2; i < n; i++) {
+            if (n % i == 0)
+                return false;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        MyClass test = new MyClass();
+        Scanner userInput = new Scanner(System.in);
+        System.out.print("Enter the number of primes to be displayed: ");
+        int num = userInput.nextInt();
+
+        int count = 0;
+        for (int i = 2; count < num; i++) {
+            if (test.isPrime(i)) {
+                System.out.print(i + " ");
+                count++;
+            }
+
+        }
+    }
+}
